613e61ab22cd8db01feb4ea13bc2d6aec9b9f4ad
nateps <nate@nateps.com>
2011-04-08 21:30:24 -0700

    Copied vers framework code out of initial demo app.

---
+++ index.js
@@ -0,0 +1,3 @@
+// This file is needed, since browserify only knows how to find modules by
+// looking for a file named index.js in the root directory of the module name
+module.exports = require("./lib/vers");
