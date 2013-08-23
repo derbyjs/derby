c2b26eafb32b4dc109c405952927e5d7e8016c23
Nate Smith <nate@nateps.com>
2013-08-22 18:12:19 -0700

    fix bugs in last commit

--- bin/bare-js/lib/server/index.js
+++ bin/bare-js/lib/server/index.js
@@ -10,8 +10,8 @@ var expressApp = module.exports = express();
 
 var mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/project';
 var store = derby.createStore({
-  db: liveDbMongo(mongoUrl + '?auto_reconnect', safe: true)
-, redis: require('redis').createClient();
+  db: liveDbMongo(mongoUrl + '?auto_reconnect', {safe: true})
+, redis: require('redis').createClient()
 });
 
 function createUserId(req, res, next) {
