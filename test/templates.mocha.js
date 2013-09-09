13aa8576ff1cf12dcddcbe55419989f38cd8312b
Nate Smith <nate@nateps.com>
2013-09-09 14:47:16 -0700

    add block tests

--- test/templates.mocha.js
+++ test/templates.mocha.js
@@ -10,6 +10,8 @@ var data = {
   , zero: 0
   , yep: true
   , nope: false
+  , nada: null
+  , letters: ['A', 'B', 'C']
   }
 };
 var objectModel = new expressions.ObjectModel(data);
@@ -51,10 +53,23 @@ describe('Parse and render dynamic text and blocks', function() {
 
   it('Value within text', function() {
     test('Say, "{{_page.greeting}}"', 'Say, "Howdy!"');
+    test('{{_page.zero}}', '0');
+    test('{{_page.nope}}', 'false');
+    test('{{_page.yep}}', 'true');
+    test('{{_page.nada}}', '');
+    test('{{nothing}}', '');
+  });
+
+  it('With block', function() {
+    test('{{with _page.yep}}yes{{/with}}', 'yes');
+    test('{{with _page.nope}}yes{{/with}}', 'yes');
+    test('{{with _page.yep}}{{this}}{{/with}}', 'true');
+    test('{{with _page.nope}}{{this}}{{/with}}', 'false');
   });
 
   it('If block', function() {
     test('{{if _page.yep}}yes{{/if}}', 'yes');
+    test('{{if _page.yep}}{{this}}{{/if}}', 'true');
     test('{{if _page.nope}}yes{{/if}}', '');
     test('{{if nothing}}yes{{/if}}', '');
   });
@@ -62,13 +77,26 @@ describe('Parse and render dynamic text and blocks', function() {
   it('Unless block', function() {
     test('{{unless _page.yep}}yes{{/unless}}', '');
     test('{{unless _page.nope}}yes{{/unless}}', 'yes');
+    test('{{unless _page.nope}}{{this}}{{/unless}}', 'false');
     test('{{unless nothing}}yes{{/unless}}', 'yes');
   });
 
-  it('If else block', function() {
+  it('Else block', function() {
     test('{{if _page.yep}}yes{{else}}no{{/if}}', 'yes');
     test('{{if _page.nope}}yes{{else}}no{{/if}}', 'no');
     test('{{if nothing}}yes{{else}}no{{/if}}', 'no');
+  });
+
+  it('Else if block', function() {
+    test('{{if _page.yep}}1{{else if _page.yep}}2{{else}}3{{/if}}', '1');
+    test('{{if _page.nope}}1{{else if _page.yep}}2{{else}}3{{/if}}', '2');
+    test('{{if _page.nope}}1{{else if _page.yep}}{{this}}{{else}}3{{/if}}', 'true');
+    test('{{if _page.nope}}1{{else if _page.nope}}2{{else}}3{{/if}}', '3');
+  });
+
+  it('Each block', function() {
+    test('{{each _page.letters}}{{this}}:{{/each}}', 'A:B:C:');
+    // test('{{each [1, 2, 3]}}{{this * 2}}{{/each}}', '246');
   });
 
 });
