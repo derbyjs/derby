c49fae2db5385dd39739d1e9cc57d1452f764be9
Joseph Gentle <me@josephg.com>
2013-09-12 16:25:50 -0700

    Fixed support for chained array references

--- test/eventmodel.mocha.js
+++ test/eventmodel.mocha.js
@@ -8,17 +8,30 @@ describe('eventmodel', function() {
       x: 1,
       list: [1,2,3],
       objList: [
-        {url:'1', listName:'one'},
-        {url:'2', listName:'two'}
+        {url:'/0', listName:'one'},
+        {url:'/1', listName:'two'},
+        {url:'/2', listName:'three'}
       ]
     };
 
     this.model = new ObjectModel(this.data);
     this.em = new EventModel();
 
+    var self = this;
+
+    // This is a helper to update the data model and trigger EM bindings in one go.
+    this.set = function(segments, value) {
+      var d = self.data;
+      for (var i = 0; i < segments.length - 1; i++) {
+        d = d[segments[i]];
+      }
+      d[segments[segments.length - 1]] = value;
+
+      self.em.setAt(segments);
+    }
+
     // Lots of these tests need to check that a binding is called. This isn't
     // bound or anything, but its really handy code.
-    var self = this;
     this.bindingCalled = 0;
     this.binding = {
       update:function() {
@@ -35,8 +48,7 @@ describe('eventmodel', function() {
       var ref = this.em.arrayLookup(this.model, ['list'], ['x']);
       this.em.addBinding(['list', ref], this.binding);
 
-      this.data.x = 0;
-      this.em.setAt(['x']);
+      this.set(['x'], 0);
 
       assert.equal(this.bindingCalled, 1);
     });
@@ -45,10 +57,8 @@ describe('eventmodel', function() {
       var ref = this.em.arrayLookup(this.model, ['list'], ['x']);
       this.em.addBinding(['list', ref], this.binding);
 
-      this.data.list[1] = 10;
-      this.em.setAt(['list', 1]);
+      this.set(['list', 1], 10);
       assert.equal(this.bindingCalled, 1);
-      //console.log(JSON.stringify(this.em, null, 2));
     });
 
     it('reuses the array reference if we call arrayLookup again', function() {
@@ -60,8 +70,7 @@ describe('eventmodel', function() {
     it('reuses the array reference if we call arrayLookup after moving the inner value', function() {
       var ref1 = this.em.arrayLookup(this.model, ['list'], ['x']);
 
-      this.data.x = 0;
-      this.em.setAt(['x']);
+      this.set(['x', 0]);
       var ref2 = this.em.arrayLookup(this.model, ['list'], ['x']);
       
       assert.strictEqual(ref1, ref2);
@@ -70,14 +79,33 @@ describe('eventmodel', function() {
     it('reuses the array reference if we call arrayLookup after moving the outer value', function() {
       var ref1 = this.em.arrayLookup(this.model, ['list'], ['x']);
 
-      this.data.list[1] = 10;
-      this.em.setAt(['list', 1]);
+      this.set(['list', 1], 10);
       var ref2 = this.em.arrayLookup(this.model, ['list'], ['x']);
       
       assert.strictEqual(ref1, ref2);
     });
 
+    it('allows chained references', function() {
+      // For this test we'll try and access objList[list[x]].listName. x is 1,
+      // list[1] is 2 and objList[2].listName is 'three'.
+
+      var ref1 = this.em.arrayLookup(this.model, ['list'], ['x']);
+      var ref2 = this.em.arrayLookup(this.model, ['objList'], ['list', ref1]);
+
+      this.em.addBinding(['objList', ref2, 'listName'], this.binding);
+
+      this.set(['objList', 2, 'listName'], 'internet');
+      assert.strictEqual(this.bindingCalled, 1);
+
+      this.set(['list', 1], 0);
+      assert.strictEqual(this.bindingCalled, 2);
+
+      this.set(['x'], 0);
+      assert.strictEqual(this.bindingCalled, 3);
+    });
   });
+
+
   describe('expandSegments', function() {
     it('passes through primitive values', function() {
       assert.deepEqual(EventModel.expandSegments([1, 2, 3, 'a', 'b', 'c']), [1, 2, 3, 'a', 'b', 'c']);
@@ -93,8 +121,7 @@ describe('eventmodel', function() {
       var ref = this.em.arrayLookup(this.model, ['objList'], ['x']);
       assert.deepEqual(EventModel.expandSegments(['objList', ref, 'url']), ['objList', 1, 'url']);
 
-      this.data.x = 2;
-      this.em.setAt(['x']);
+      this.set(['x'], 2);
 
       assert.deepEqual(EventModel.expandSegments(['objList', ref, 'url']), ['objList', 2, 'url']);
     });
