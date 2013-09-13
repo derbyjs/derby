06907e6e4c2c98dda04e2889d90571dd0939639d
Joseph Gentle <me@josephg.com>
2013-09-13 14:14:25 -0700

    Added more tests and fixed a couple of small bugs

--- test/eventmodel.mocha.js
+++ test/eventmodel.mocha.js
@@ -32,25 +32,67 @@ describe('eventmodel', function() {
 
     // Lots of these tests need to check that a binding is called. This isn't
     // bound or anything, but its really handy code.
-    this.bindingCalled = 0;
+    this.updateCalled = 0;
+    this.insertCalled = 0;
+    this.insertArgs = null;
+
     this.binding = {
-      update:function() {
-        self.bindingCalled++;
-      }
+      update:function() {self.updateCalled++;},
+      insert:function(index, howMany) {
+        self.insertCalled++;
+        this.insertArgs = {index:index, howMany:howMany};
+      },
     };
   });
 
   it('updates any object references under a path when remove/insert/move happens');
 
-  describe('array lookup', function() {
+  describe('sets', function() {
+    it('updates a binding when its value changes', function() {
+      this.em.addBinding(['x'], this.binding);
+      this.set(['x'], 10);
 
+      assert.equal(this.updateCalled, 1);
+    });
+
+    it('updates a fixed list element binding', function() { 
+      this.em.addBinding(['list', 1], this.binding);
+      this.set(['list', 1], 10);
+
+      assert.equal(this.updateCalled, 1);
+    });
+
+    it('updates bound children when the parent is replaced', function() {
+      this.em.addBinding(['list', 1], this.binding);
+      this.set(['list'], [4,5,6]);
+
+      assert.equal(this.updateCalled, 1);
+    });
+
+    it('lets you bind to places with currently undefined values', function() {
+      this.em.addBinding(['list', 10], this.binding);
+      this.set(['list', 10], 'hi');
+
+      assert.equal(this.updateCalled, 1);
+    });
+  });
+
+  describe('lists', function() {
+    it('Does not update an item binding inside a list item when something is inserted around it', function() {
+      //var ctx = {item:1};
+      //this.em.addBinding(['list', ctx], this.binding);
+
+    });
+  });
+
+  describe('array lookup', function() {
     it('updates a binding if the index changes value', function() {
       var ref = this.em.arrayLookup(this.model, ['list'], ['x']);
       this.em.addBinding(['list', ref], this.binding);
 
       this.set(['x'], 0);
 
-      assert.equal(this.bindingCalled, 1);
+      assert.equal(this.updateCalled, 1);
     });
 
     it('updates a binding if the resolved path changes', function() {
@@ -58,7 +100,7 @@ describe('eventmodel', function() {
       this.em.addBinding(['list', ref], this.binding);
 
       this.set(['list', 1], 10);
-      assert.equal(this.bindingCalled, 1);
+      assert.equal(this.updateCalled, 1);
     });
 
     it('reuses the array reference if we call arrayLookup again', function() {
@@ -95,13 +137,39 @@ describe('eventmodel', function() {
       this.em.addBinding(['objList', ref2, 'listName'], this.binding);
 
       this.set(['objList', 2, 'listName'], 'internet');
-      assert.strictEqual(this.bindingCalled, 1);
+      assert.strictEqual(this.updateCalled, 1);
 
       this.set(['list', 1], 0);
-      assert.strictEqual(this.bindingCalled, 2);
+      assert.strictEqual(this.updateCalled, 2);
 
       this.set(['x'], 0);
-      assert.strictEqual(this.bindingCalled, 3);
+      assert.strictEqual(this.updateCalled, 3);
+
+      // Going back out again to make sure that all the bindings have been updated correctly.
+      this.set(['list', 0], 0);
+      assert.strictEqual(this.updateCalled, 4);
+
+      this.set(['objList', 0, 'listName'], 'superman');
+      assert.strictEqual(this.updateCalled, 5);
+
+      // Some things that should not update the binding.
+      this.set(['objList', 2, 'listName'], 'blah');
+      this.set(['objList', 1, 'listName'], 'superduper');
+      this.set(['list', 1], 1);
+      assert.strictEqual(this.updateCalled, 5);
+    });
+
+    it('lets you bind to a property in an object', function() {
+      this.set(['x'], 'url');
+      var ref = this.em.arrayLookup(this.model, ['objList', 1], ['x']);
+
+      this.em.addBinding(['objList', 1, ref], this.binding);
+
+      this.set(['objList', 1, 'url'], 'http://example.com');
+      assert.strictEqual(this.updateCalled, 1);
+
+      this.set(['x'], 'listName');
+      assert.strictEqual(this.updateCalled, 2);
     });
   });
 
