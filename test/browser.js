73a94a0ea725e6346fd8ed603fc96d77705106b1
Nate Smith <nate@nateps.com>
2013-10-18 00:05:16 -0700

    simplify running browser tests

--- test/browser.js
+++ test/browser.js
@@ -1,14 +1,2 @@
 require('./expression.mocha.js');
 require('./templates.mocha.js');
-
-// var derby = require('derby');
-// var app = derby.createApp();
-// var page = app.createPage();
-
-// var templates = document.querySelectorAll('script[type="text/template"]');
-// for (var i = 0; i < templates.length; i++) {
-//   var template = templates[i];
-//   app.views.register(template.dataset.name, template.innerHTML, template.dataset);
-// }
-
-// document.body.appendChild(page.getFragment('body'));
---
+++ test/server.js
@@ -0,0 +1,40 @@
+var fs = require('fs');
+var path = require('path');
+var express = require('express');
+var bundle = require('racer/lib/bundle');
+
+var app = express();
+
+app
+  .use(express.static('../node_modules/mocha'))
+  .use(express.static('../node_modules/expect.js'))
+  .use(express.static(__dirname))
+
+app.get('/test/:filename', function(req, res, next) {
+  var file = __dirname + '/' + req.params.filename;
+  sendBundle(file, res, next);
+});
+
+app.get('/derby-standalone', function(req, res, next) {
+  var file = path.dirname(__dirname) + '/lib/standalone';
+  sendBundle(file, res, next);
+});
+
+function sendBundle(file, res, next) {
+  var bundleOptions = {
+    minify: false
+  , configure: function(b) {
+      b.add(file);
+    }
+  };
+  bundle(bundleOptions, function(err, code, map) {
+    if (err) return next(err);
+    res.type('js');
+    res.send(code);
+  });
+}
+
+var port = process.env.PORT || 7777;
+app.listen(port, function(err) {
+  console.log('Derby test server running at http://localhost:' + port);
+});
