24c7b1be705b779402a87a1a3f7162ac4fe046b9
Nate Smith <nate@lever.co>
2019-11-04 12:53:43 -0800

    Compnent definition: Specify `is`, `file`, `source`, and `options` via view object
    Make use of view object exclulsive of other means of specifying the `is` and `file`, because these may be inherited when extending another class. Thus, use of `view` object form only is most clear.

--- test/all/ComponentHarness.mocha.js
+++ test/all/ComponentHarness.mocha.js
@@ -6,19 +6,19 @@ describe('ComponentHarness', function() {
   describe('getInstance', function() {
     it('creates a component instance', function() {
       function Box() {}
-      Box.is = 'box';
+      Box.view = {is: 'box'};
       var box = new ComponentHarness('<view is="box" />', Box).getInstance();
       expect(box).instanceof(Box);
     });
     it('Can pass a value to a component instance from harness template', function() {
       function Box() {}
-      Box.is = 'box';
+      Box.view = {is: 'box'};
       var box = new ComponentHarness('<view is="box" value="Hi" />', Box).getInstance();
       expect(box.model.get('value')).equal('Hi');
     });
     it('Can pass a value to a component instance from harness model', function() {
       function Box() {}
-      Box.is = 'box';
+      Box.view = {is: 'box'};
       var harness = new ComponentHarness('<view is="box" value="{{_page.message}}" />', Box);
       harness.model.set('_page.message', 'Yo.');
       var box = harness.getInstance();
@@ -29,8 +29,10 @@ describe('ComponentHarness', function() {
   describe('getHtml', function() {
     it('Returns HTML', function() {
       function Box() {}
-      Box.is = 'box';
-      Box.view = {source: '<div class="box"></div>'};
+      Box.view = {
+        is: 'box',
+        source: '<div class="box"></div>'
+      };
       var html = new ComponentHarness('<view is="box" />', Box).getHtml();
       expect(html).equal('<div class="box"></div>');
     });
@@ -41,8 +43,10 @@ describe('ComponentHarness', function() {
 
     it('Returns a DocumentFragment', function() {
       function Box() {}
-      Box.is = 'box';
-      Box.view = {source: '<div class="box"></div>'};
+      Box.view = {
+        is: 'box',
+        source: '<div class="box"></div>'
+      };
       var fragment = new ComponentHarness('<view is="box" />', Box).getFragment();
       expect(fragment).instanceof(DocumentFragment);
       expect(fragment).html('<div class="box"></div>');
