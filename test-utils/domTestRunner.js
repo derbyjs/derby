7fd531ce9b54ebd7768165992d56ba04c24a8c0a
Eric Hwang <eric.hwang@lever.co>
2020-06-24 12:46:17 -0700

    test-utils: Support multiple harnesses in runner page cleanup

--- test-utils/domTestRunner.js
+++ test-utils/domTestRunner.js
@@ -15,6 +15,7 @@ exports.DomTestRunner = DomTestRunner;
 function DomTestRunner() {
   this.window = null;
   this.document = null;
+  this.harnesses = [];
 }
 
 DomTestRunner.prototype.installMochaHooks = function(options) {
@@ -36,7 +37,7 @@ DomTestRunner.prototype.createHarness = function() {
   if (arguments.length > 0) {
     harness.setup.apply(harness, arguments);
   }
-  runner._harness = harness;
+  this.harnesses.push(harness);
   return harness;
 };
 
@@ -64,12 +65,12 @@ function mochaHooksForNode(runner, options) {
   global.afterEach(function() {
     // Destroy the pages created by the harness, so that if a test cleans up its model itself,
     // bindings won't throw errors due to `document` not being present.
-    if (runner._harness) {
-      runner._harness.app.pages.forEach(function(page) {
+    runner.harnesses.forEach(function(harness) {
+      harness.app._pages.forEach(function(page) {
         page.destroy();
       });
-    }
-    runner._harness = null;
+    });
+    runner.harnesses = [];
 
     jsdom.window.close();
     runner.window = null;
