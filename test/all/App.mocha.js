819d1ecc8d17aa12fded8550aba52f567567a059
Craig Beck <craig.beck@lever.co>
2022-03-30 11:45:45 -0700

    Mollify linter

--- test/all/App.mocha.js
+++ test/all/App.mocha.js
@@ -1,23 +1,23 @@
-const expect = require('chai').expect;
-const App = require('../../lib/App');
+var expect = require('chai').expect;
+var App = require('../../lib/App');
 
 describe('App._parseInitialData', () => {
   it('parses simple json', () => {
-    const actual = App._parseInitialData('{"foo": "bar"}');
+    var actual = App._parseInitialData('{"foo": "bar"}');
     expect(actual).to.deep.equal({ foo: 'bar' });
   });
 
   it('parses escaped json', () => {
-    const actual = App._parseInitialData('{"foo": "<\\u0021bar><\\/bar>"}');
+    var actual = App._parseInitialData('{"foo": "<\\u0021bar><\\/bar>"}');
     expect(actual).to.deep.equal({ foo: '<!bar></bar>' });
   });
 
   it('thorws error with context for unexpected tokens', () => {
     try {
-      const actual = App._parseInitialData('{"foo": b}');
+      var actual = App._parseInitialData('{"foo": b}');
     } catch (error) {
       expect(error.message).to.equal(
-        'Parse failure: Unexpected token b in JSON at position 8 context: \'{"foo": b}\'',
+        'Parse failure: Unexpected token b in JSON at position 8 context: \'{"foo": b}\''
       );
     }
   });
