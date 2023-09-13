2a127a50fd0394c5963aedf06d652822cbf59560
Craig Beck <luckybonza@gmail.com>
2023-09-13 12:15:28 -0700

    Dont alias DependencyOptions

--- src/templates/index.ts
+++ src/templates/index.ts
@@ -2,4 +2,4 @@ export * as contexts from './contexts';
 export * as expressions from './expressions';
 export * as operatorFns from './operatorFns';
 export * as templates from './templates';
-export { DependencyOptions as options} from './dependencyOptions';
+export { DependencyOptions } from './dependencyOptions';
