02544aa83f46723467b54daf413dc3674e758479
Craig Beck <luckybonza@gmail.com>
2023-09-25 12:39:57 -0700

    Lift Component to base class

--- src/Derby.ts
+++ src/Derby.ts
@@ -11,13 +11,13 @@ import { Component } from './components';
 import { Page } from './Page';
 
 export abstract class DerbyBase extends Racer {
+  Component = Component;
   abstract createApp(name: string, filename: string, options): AppBase
 }
 
 export class Derby extends DerbyBase {
   App = App;
   Page = Page;
-  Component = Component;
 
   createApp(name: string, filename: string, options) {
     return new this.App(this, name, filename, options);
