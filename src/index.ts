8310868a93a5e70eab685cd25f518219e4cf4f3e
Craig Beck <luckybonza@gmail.com>
2024-01-18 15:32:58 -0800

    Export App, PageParam types

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { util } from 'racer';
 
+import { type App } from './App';
 import { Derby, type DerbyBase } from './Derby';
 import { type Page } from './Page';
+import { type PageParams } from './routes';
 
 const DerbyClass = util.isServer
   ? util.serverRequire(module, './DerbyForServer').DerbyForServer
@@ -12,9 +14,10 @@ const instance: DerbyBase = new DerbyClass();
 const { Component } = instance;
 
 export {
-  // App,
+  App,
   Component,
   Derby,
   instance,
   Page,
+  PageParams,
 }
