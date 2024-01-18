b3f108fe33a1646c893dac7259847d6a91dd65f9
Craig Beck <luckybonza@gmail.com>
2024-01-18 13:52:05 -0800

    Export as namespace

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import { util } from 'racer';
 
-import { Derby } from './Derby';
+import { Derby, type DerbyBase } from './Derby';
 
 
 const DerbyClass = util.isServer
   ? util.serverRequire(module, './DerbyForServer').DerbyForServer
   : Derby;
 console.log('class', DerbyClass);
-export = new DerbyClass();
+const instance: DerbyBase = new DerbyClass();
+
+export {
+  Derby,
+  instance,
+}
